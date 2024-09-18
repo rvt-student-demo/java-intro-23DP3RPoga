a6f26b44c0b9f6daa02ff12757aba727321631e4
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-09-18 06:58:35 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,32 +6,93 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        
+        // System.out.println("Ievadiet savu vardu");
+        // String name = scanner.nextLine();
+        // System.out.println("Ievadiet savu uzvardu");
+        // String lastname = scanner.nextLine();
+        // System.out.println("Ievadiet savu grupu");
+        // String group = scanner.nextLine();
 
-        System.out.println("Ievadiet savu vardu");
-        String name = scanner.nextLine();
-        System.out.println("Ievadiet savu uzvardu");
-        String lastname = scanner.nextLine();
-        System.out.println("Ievadiet savu grupu");
-        String group = scanner.nextLine();
-
-        //Izvade vienā rindiņā izmantojot 3 mainīgos
-        //Output example: Students: <name> <surname>, grupa: <group>
-        System.out.println("Students: " + name + " "
-        + lastname + ", grupa: " + group);
-
-
+        // //Izvade vienā rindiņā izmantojot 3 mainīgos
+        // //Output example: Students: <name> <surname>, grupa: <group>
+        // System.out.println("Students: " + name + " "
+        // + lastname + ", grupa: " + group);
 
 
 
+        // int chicken=9000;
+        // double bacon= 0.1;
+        // String tractor="Zetor";
+
+        // System.out.println("Chicken:");
+        // System.out.println(chicken);
+        // System.out.println("Bacon (kg):");
+        // System.out.println(bacon);
+        // System.out.println("Tractor:");
+        // System.out.println(tractor);
+
+        // System.out.println();
+        // System.out.println("And finally, a summary:");
+        // System.out.println(chicken);
+        // System.out.println(bacon);
+        // System.out.println(tractor);
+
+        // double sk = 12;21
+        // System.out.println(sk);
+        // sk = 1;
+        // System.out.println(sk);
+        // String vardi= "ben";
+        // System.out.println(vardi);
+        // vardi="seven";
+        // System.out.println(vardi);
+        
+            // System.out.println("sum: "+(10+20+30));
+
+        // System.out.println("Ievadiet skaitli");
+        // int number1=Integer.valueOf(scanner.nextLine());
+        // System.out.println("Ievadiet skaitli");
+        // int number2=Integer.valueOf(scanner.nextLine());
+        // System.out.println(number1+number2);
+        // System.out.println(number1*number2);
+        // System.out.println((double)number1/number2);
+        // System.out.println(number1/ Double.valueOf(number2));
+        // boolean boolInput=Boolean.valueOf(scanner.nextLine());
+        // double doubleInput=Double.valueOf(scanner.nextLine());
+        
+        // double result=3/(double)2;
+        // System.out.println(result);
+        // int first=2;
+        // int second=3;
+        // double rezult=(second/(double)first);
+        // System.out.println(rezult);
 
 
 
+        //Simple calculator
 
+        System.out.println("Give first number: ");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give second number: ");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(first+" + "+second+" = "+(first+second));
+        System.out.println(first+" - "+second+" = "+(first-second));
+        System.out.println(first+" * "+second+" = "+(first*second));
+        System.out.println(first+" / "+second+" = "+((first+(double)second)/2));
 
 
 
 
         
+        
+        
+        
+        // System.out.println(10/(double)3);
+
+
+
         // String concatenation
         // System.out.println("Students: " + name + " "
         // + lastname + ", grupa: " + group);
