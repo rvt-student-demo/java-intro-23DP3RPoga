9dafbd75e802d29401e67489a501f21f3998b299
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-09-20 07:53:57 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,143 +7,35 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         
-
-
-        // Speeding ticket
-        System.out.println("Give speed: ");
+        // Grades and Points
+        System.out.println("Give points [0-100]:");
         int intInput=Integer.valueOf(scanner.nextLine());
-         if (intInput>120) {
-                System.out.println("Speeding ticket!");
-            }   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // System.out.println("Ievadiet savu vardu");
-        // String name = scanner.nextLine();
-        // System.out.println("Ievadiet savu uzvardu");
-        // String lastname = scanner.nextLine();
-        // System.out.println("Ievadiet savu grupu");
-        // String group = scanner.nextLine();
-
-        // //Izvade vienā rindiņā izmantojot 3 mainīgos
-        // //Output example: Students: <name> <surname>, grupa: <group>
-        // System.out.println("Students: " + name + " "
-        // + lastname + ", grupa: " + group);
-
-
-
-        // int chicken=9000;
-        // double bacon= 0.1;
-        // String tractor="Zetor";
-
-        // System.out.println("Chicken:");
-        // System.out.println(chicken);
-        // System.out.println("Bacon (kg):");
-        // System.out.println(bacon);
-        // System.out.println("Tractor:");
-        // System.out.println(tractor);
-
-        // System.out.println();
-        // System.out.println("And finally, a summary:");
-        // System.out.println(chicken);
-        // System.out.println(bacon);
-        // System.out.println(tractor);
-
-        // double sk = 12;21
-        // System.out.println(sk);
-        // sk = 1;
-        // System.out.println(sk);
-        // String vardi= "ben";
-        // System.out.println(vardi);
-        // vardi="seven";
-        // System.out.println(vardi);
-        
-            // System.out.println("sum: "+(10+20+30));
-
-        // System.out.println("Ievadiet skaitli");
-        // int number1=Integer.valueOf(scanner.nextLine());
-        // System.out.println("Ievadiet skaitli");
-        // int number2=Integer.valueOf(scanner.nextLine());
-        // System.out.println(number1+number2);
-        // System.out.println(number1*number2);
-        // System.out.println((double)number1/number2);
-        // System.out.println(number1/ Double.valueOf(number2));
-        // boolean boolInput=Boolean.valueOf(scanner.nextLine());
-        // double doubleInput=Double.valueOf(scanner.nextLine());
-        
-        // double result=3/(double)2;
-        // System.out.println(result);
-        // int first=2;
-        // int second=3;
-        // double rezult=(second/(double)first);
-        // System.out.println(rezult);
-
-
-
-        //Simple calculator
-
-        // System.out.println("Give first number: ");
-        // int first = Integer.valueOf(scanner.nextLine());
-
-        // System.out.println("Give second number: ");
-        // int second = Integer.valueOf(scanner.nextLine());
-
-        // System.out.println(first+" + "+second+" = "+(first+second));
-        // System.out.println(first+" - "+second+" = "+(first-second));
-        // System.out.println(first+" * "+second+" = "+(first*second));
-        // System.out.println(first+" / "+second+" = "+((first+(double)second)/2));
-
-
-
-            // int first = (1 + 1);//2
-            // int second = first + 3 * (2 + 5);//23
-        
-            // first = 5;
-        
-            // int third = first + second;//28
-            // System.out.println(first);//5
-            // System.out.println(second);//23
-            // System.out.println(third);//28
-        
-        
-        
-        
-        // System.out.println(10/(double)3);
-
-
-
-        // String concatenation
-        // System.out.println("Students: " + name + " "
-        // + lastname + ", grupa: " + group);
-        
-        // //Refernce data type
-        // String name = "Ben";
-        // // Primitive data type
-        // int age = 21;
-        // // Primitive data type
-        // boolean isOnline = true;
-
-        // System.out.println(name); 
-        // System.out.println(age);
-        // System.out.println(isOnline) ;
-        // System.out.println(scanner);
-        // // Ctrl + /
-        // /* qwerty */
-
-
+        if (intInput<0) {
+            System.out.println("Grade:impossible!");
+        }
+        else if (intInput>=0&& intInput<=49) {
+            System.out.println("Grade: failed");
+        }
+        else if (intInput>=50&& intInput<=59) {
+            System.out.println("Grade: 1");
+        }
+        else if (intInput>=60&& intInput<=69) {
+            System.out.println("Grade: 2");
+        }
+        else if (intInput>=70&& intInput<=79) {
+            System.out.println("Grade: 3");
+        }
+        else if (intInput>=80&& intInput<=89) {
+            System.out.println("Grade: 4");
+        }
+        else if (intInput>=90&& intInput<=100) {
+            System.out.println("Grade: 5");
+        }
+        else if (intInput>100) {
+            System.out.println("Grade: incredible!");
+        }
+      
     }
 
 }
