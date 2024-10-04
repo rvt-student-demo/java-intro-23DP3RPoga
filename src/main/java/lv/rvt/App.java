86b42ec3d34908be2bbf311b0259806a75ece4ec
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-10-04 13:25:46 +0000

    Sum of a sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,18 +5,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Counting
+        //Sum of a sequence
         Scanner scanner = new Scanner(System.in);
-        int input=Integer.valueOf(scanner.nextLine());
-        
-        int output=0;
-        while (input>=0) {
-            System.out.println(output);
-            output=output+1;
-            input=input-1;
+        System.out.println("1+2+3+...+n Input last number");
+        int n=Integer.valueOf(scanner.nextLine());
+        //1+2+3...+  n
+        int sum=0;
+        int loop=n;
+        while (loop>=0) {
+            sum=sum+n;
+            n--;
+            loop--;
             
         }
-            
-    }
+        System.out.println(sum);   
+        }       
 }
 
