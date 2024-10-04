3d157e1d12710bef2c494f766950bf765850460a
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-10-04 12:38:24 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,37 +5,18 @@ public class App
 {
     public static void main( String[] args )
     {
+        //Counting
         Scanner scanner = new Scanner(System.in);
+        int input=Integer.valueOf(scanner.nextLine());
         
-        // Grades and Points
-        System.out.println("Give points [0-100]:");
-        int intInput=Integer.valueOf(scanner.nextLine());
-
-        if (intInput<0) {
-            System.out.println("Grade:impossible!");
+        int output=0;
+        while (input>=0) {
+            System.out.println(output);
+            output=output+1;
+            input=input-1;
+            
         }
-        else if (intInput>=0&& intInput<=49) {
-            System.out.println("Grade: failed");
-        }
-        else if (intInput>=50&& intInput<=59) {
-            System.out.println("Grade: 1");
-        }
-        else if (intInput>=60&& intInput<=69) {
-            System.out.println("Grade: 2");
-        }
-        else if (intInput>=70&& intInput<=79) {
-            System.out.println("Grade: 3");
-        }
-        else if (intInput>=80&& intInput<=89) {
-            System.out.println("Grade: 4");
-        }
-        else if (intInput>=90&& intInput<=100) {
-            System.out.println("Grade: 5");
-        }
-        else if (intInput>100) {
-            System.out.println("Grade: incredible!");
-        }
-      
+            
     }
-
 }
+
