895c6c8f3f6ae001cbf2a85c1d05a9f4dc61fa79
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-10-11 08:26:04 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,11 @@
 package lv.rvt;
-import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        //Sum of a sequence
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("1+2+3+...+n Input last number");
-        int n=Integer.valueOf(scanner.nextLine());
-        //1+2+3...+  n
-        int sum=0;
-        int loop=n;
-        while (loop>=0) {
-            sum=sum+n;
-            n--;
-            loop--;
-            
-        }
-        System.out.println(sum);   
+
+        Stars.printRighLeaningTriangle(5);
         }       
 }
 
---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,93 @@
+package lv.rvt;
+
+public class Stars {
+
+    public static void printStars(int number){
+
+        for  (int i = 0; i < number; i++) {
+            
+            System.out.print("*");
+            
+
+
+            
+            
+        }
+        System.out.println("");
+
+      
+    }
+    public static void printSquare(int size){
+        for  (int i = 0; i < size; i++){
+            printStars(size);
+        }
+    }
+    public static void printRectangle(int width, int height){
+        while (height>0) {
+            
+            for  (int i = 0; i < width; i++){
+                System.out.print("*");
+                }
+            System.out.println("");
+            height=height-1;
+        }
+    }
+    public static void printTriangle(int size){
+        int number4=1; //    output=
+        // int number4=1;    output=*
+        // int number4=2;    output=**
+        // int number4=3;    output=***
+        // int number4=size; output=****
+        while (number4!=size+1 ) {
+            
+                printStars(number4);
+            number4=number4+1;
+        }
+
+    }
+    public static void printSpaces(int number){
+        for  (int i = 0; i < number; i++) {
+            
+            System.out.print(" ");
+            
+        }
+    }
+        public static void printRighLeaningTriangle(int size){
+            int number4=1;
+            int number5=size-1; //    output=
+            // int number4=1;    output=   *    3
+            // int number4=2;    output=  **    2
+            // int number4=3;    output= ***    1
+            // int number4=size; output=****
+            while (number4!=size+1 ) {
+                    printSpaces(number5);
+                    printStars(number4);
+                number4=number4+1;
+                number5=number5-1;
+            }
+    }
+    public static void christmasTree(int height){
+
+                                //height 4
+    //      *                   space 3 stars 1
+    //     ***                  space 2 stars 3
+    //    *****                 space 1 stars 5
+    //   *******                space 0 stars 7
+
+
+    //     ***                  space  2 stars 3
+    //     ***                  space  2 stars 3
+
+
+        while (condition) {
+            
+        }
+        printSpaces();
+        printStars(3);
+        printSpaces();
+        printStars(3);
+    }
+}   
+
+    
+
