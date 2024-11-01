99af4cb8660338ab8439b3a168d487cbb7c2c498
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-11-01 09:27:23 +0000

    Personal Details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,38 @@
 package lv.rvt;
+import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.Scanner;
 
-public class App 
-{
+public class App { 
+
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name and birthyear: ");
+        ArrayList<String> dati = new ArrayList<String>();
 
-        Stars.christmasTree(100);
-        }       
+            while (scanner.hasNextLine()) {
+              String i=scanner.nextLine();
+              dati.add(i);//input
+              if (i.equals(""));//beidzas input
+              {
+                String[]vardi = new String[dati.size()];
+                String[]gadi = new String[dati.size()];
+                for (int j = 0; j < dati.size(); j++) {
+                  vardi[j] = dati.get(j).split(",")[0];
+                  gadi[j] = dati.get(j).split(",")[1];
+                  
+                }
+                System.out.println(gadi);
+                System.out.println(vardi);
+              }
+
+
+    
+      }
+       
+    }
+    
+
 }
-
