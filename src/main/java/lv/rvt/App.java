b140882f2e7d8e3e9c50e60dea7877f054ce39d6
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-11-13 07:32:55 +0000

    Person

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,42 +1,21 @@
 package lv.rvt;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class App { 
 
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name and birthyear: ");
-        ArrayList<String> dati = new ArrayList<String>();
+public static void main(String[] args) {
+    Person matti = new Person("Matti");
+    Person juhana = new Person("Juhana");
 
-            while (scanner.hasNextLine()) {
-              String i=scanner.nextLine();
-              if (i.equals(""))
-                {
-                break;
-                }
-              dati.add(i);
-            String[] vardi = new String[dati.size()];
-            String[] gadi = new String[dati.size()];
-              
-                for (int j = 0; j < dati.size(); j++) {
-            String[] parts = dati.get(j).split(",");
-            if (parts.length == 2) { 
-                vardi[j] = parts[0];
-                gadi[j] = parts[1];
-            } else {
-                System.out.println("Invalid format for entry: " + dati.get(j));
-            }
-        }
+    matti.setHeight(180);
+    matti.setWeight(86);
 
+    juhana.setHeight(175);
+    juhana.setWeight(64);
 
-        System.out.println("Names: " + Arrays.toString(vardi));
-        System.out.println("Birth years: " + Arrays.toString(gadi));
-    }
+    System.out.println(matti.getName() + ", body mass index is " + matti.bodyMassIndex());
+    System.out.println(juhana.getName() + ", body mass index is " + juhana.bodyMassIndex());
+}
 }
 
-    
-      }
+
   
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -1,100 +1,100 @@
-package lv.rvt;
+// // package lv.rvt;
 
-public class Stars {
+// public class Stars {
 
-    public static void printStars(int number){
+//     public static void printStars(int number){
 
-        for  (int i = 0; i < number; i++) {
+//         for  (int i = 0; i < number; i++) {
             
-            System.out.print("*");
+//             System.out.print("*");
             
 
 
             
             
-        }
-        System.out.println("");
+//         }
+//         System.out.println("");
 
       
-    }
-    public static void printSquare(int size){
-        for  (int i = 0; i < size; i++){
-            printStars(size);
-        }
-    }
-    public static void printRectangle(int width, int height){
-        while (height>0) {
+//     }
+//     public static void printSquare(int size){
+//         for  (int i = 0; i < size; i++){
+//             printStars(size);
+//         }
+//     }
+//     public static void printRectangle(int width, int height){
+//         while (height>0) {
             
-            for  (int i = 0; i < width; i++){
-                System.out.print("*");
-                }
-            System.out.println("");
-            height=height-1;
-        }
-    }
-    public static void printTriangle(int size){
-        int number4=1; //    output=
-        // int number4=1;    output=*
-        // int number4=2;    output=**
-        // int number4=3;    output=***
-        // int number4=size; output=****
-        while (number4!=size+1 ) {
+//             for  (int i = 0; i < width; i++){
+//                 System.out.print("*");
+//                 }
+//             System.out.println("");
+//             height=height-1;
+//         }
+//     }
+//     public static void printTriangle(int size){
+//         int number4=1; //    output=
+//         // int number4=1;    output=*
+//         // int number4=2;    output=**
+//         // int number4=3;    output=***
+//         // int number4=size; output=****
+//         while (number4!=size+1 ) {
             
-                printStars(number4);
-            number4=number4+1;
-        }
+//                 printStars(number4);
+//             number4=number4+1;
+//         }
 
-    }
-    public static void printSpaces(int number){
-        for  (int i = 0; i < number; i++) {
+//     }
+//     public static void printSpaces(int number){
+//         for  (int i = 0; i < number; i++) {
             
-            System.out.print(" ");
+//             System.out.print(" ");
             
-        }
-    }
-        public static void printRighLeaningTriangle(int size){
-            int number4=1;
-            int number5=size-1; //    output=
-            // int number4=1;    output=   *    3
-            // int number4=2;    output=  **    2
-            // int number4=3;    output= ***    1
-            // int number4=size; output=****
-            while (number4!=size+1 ) {
-                    printSpaces(number5);
-                    printStars(number4);
-                number4=number4+1;
-                number5=number5-1;
-            }
-    }
-    public static void christmasTree(int height){
+//         }
+//     }
+//         public static void printRighLeaningTriangle(int size){
+//             int number4=1;
+//             int number5=size-1; //    output=
+//             // int number4=1;    output=   *    3
+//             // int number4=2;    output=  **    2
+//             // int number4=3;    output= ***    1
+//             // int number4=size; output=****
+//             while (number4!=size+1 ) {
+//                     printSpaces(number5);
+//                     printStars(number4);
+//                 number4=number4+1;
+//                 number5=number5-1;
+//             }
+//     }
+//     public static void christmasTree(int height){
 
-                                //height 4  
-    //      *                   space 3 stars 1
-    //     ***                  space 2 stars 3
-    //    *****                 space 1 stars 5
-                                    //space 0 stars 7
+//                                 //height 4  
+//     //      *                   space 3 stars 1
+//     //     ***                  space 2 stars 3
+//     //    *****                 space 1 stars 5
+//                                     //space 0 stars 7
 
 
-    //     ***                  space  2 stars 3
-    //     ***                  space  2 stars 3
+//     //     ***                  space  2 stars 3
+//     //     ***                  space  2 stars 3
 
-        for (int i = 0; i < height; i++) { //0 1 2 3 
-            printSpaces(height-i);
-            printStars(i*2+1);//0*2+1=
-            //3*2+1=7
+//         for (int i = 0; i < height; i++) { //0 1 2 3 
+//             printSpaces(height-i);
+//             printStars(i*2+1);//0*2+1=
+//             //3*2+1=7
         
             
-        }
-        printSpaces(height-1);
-        printStars(3);
-        printSpaces(height-1);
-        printStars(3);
+//         }
+//         printSpaces(height-1);
+//         printStars(3);
+//         printSpaces(height-1);
+//         printStars(3);
         
         
         
        
-    }   
-}
+//     }   
+// }
 
 
     
