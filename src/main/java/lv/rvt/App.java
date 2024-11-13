6ee46ad0dddaf9803fed2703a8597ef79e8e2681
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-11-13 08:42:23 +0000

    Statistics (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,21 +1,33 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App { 
 
 public static void main(String[] args) {
-    Person matti = new Person("Matti");
-    Person juhana = new Person("Juhana");
-
-    matti.setHeight(180);
-    matti.setWeight(86);
-
-    juhana.setHeight(175);
-    juhana.setWeight(64);
-
-    System.out.println(matti.getName() + ", body mass index is " + matti.bodyMassIndex());
-    System.out.println(juhana.getName() + ", body mass index is " + juhana.bodyMassIndex());
+    Scanner scanner = new Scanner(System.in);
+    Statistics sum = new Statistics();
+        Statistics odd = new Statistics();
+        Statistics even = new Statistics();
+        System.out.println("Enter numbers:");
+    while (true) {
+        int input=Integer.valueOf(scanner.nextLine());
+        sum.addNumber(input);
+        if (input%2==0)
+        even.addNumber(input);
+        else
+        odd.addNumber(input);
+        if (input==-1){
+            break;
+        }
+    }
+    int sum1=sum.sum();
+    int sum2=odd.sum();
+    int sum3=even.sum();
+    sum1+=1;
+    sum2+=1;
+    System.out.println("Sum: " + sum1);
+    System.out.println("Sum of odd numbers: "+ sum2);
+    System.out.println("Sum of even numbers: "+ sum3);
+    }  
 }
-}
-
 
   
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -1,5 +1,25 @@
 package lv.rvt;
 
 public class Statistics {
+    private int count;
+    private int sum;
 
+    public Statistics(){
+
+    }
+    public void addNumber(int number){
+      sum+=number;
+      ++count;
+
+    }
+    public int getCount() {
+        return count;
+    }
+    public int sum() {
+           return sum;
+    }
+
+    public double average() {
+        return (double)sum/count;
+    }
 }
