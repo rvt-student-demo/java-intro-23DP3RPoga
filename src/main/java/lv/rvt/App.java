ae4a639ea4fd0c5b4601b1d9c7d6899147ecf22e
23DP3RPoga <166013257+23DP3RPoga@users.noreply.github.com>
2024-11-15 08:45:30 +0000

    Payment Card

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,31 +3,27 @@ import java.util.Scanner;
 public class App { 
 
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Statistics sum = new Statistics();
-        Statistics odd = new Statistics();
-        Statistics even = new Statistics();
-        System.out.println("Enter numbers:");
-    while (true) {
-        int input=Integer.valueOf(scanner.nextLine());
-        sum.addNumber(input);
-        if (input%2==0)
-        even.addNumber(input);
-        else
-        odd.addNumber(input);
-        if (input==-1){
-            break;
-        }
-    }
-    int sum1=sum.sum();
-    int sum2=odd.sum();
-    int sum3=even.sum();
-    sum1+=1;
-    sum2+=1;
-    System.out.println("Sum: " + sum1);
-    System.out.println("Sum of odd numbers: "+ sum2);
-    System.out.println("Sum of even numbers: "+ sum3);
-    }  
-}
+    PaymentCard paulsCard = new PaymentCard(20);
+    PaymentCard mattsCard = new PaymentCard(30);
 
+    paulsCard.eatHeartily();
+    mattsCard.eatAffordably();
+
+    System.out.println("Pauls balance: " + paulsCard.toString());
+    System.out.println("Matts balance: " + mattsCard.toString());
+
+    paulsCard.addMoney(20);
+    mattsCard.eatHeartily();
+
+    System.out.println("Pauls balance: " + paulsCard.toString());
+    System.out.println("Matts balance: " + mattsCard.toString());
+
+    paulsCard.eatAffordably();
+    paulsCard.eatAffordably();
+    mattsCard.addMoney(50);
+    
+    System.out.println("Pauls balance: " + paulsCard.toString());
+    System.out.println("Matts balance: " + mattsCard.toString());
+    }
+}
   
